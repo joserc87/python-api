89bcec76d8975e6284916960c453550882e7b82b
Jose Ramon Cano Yribarren <jcano@ravenpack.com>
2022-02-22 18:37:33 +0100

    Make example generic for edge and rpa

--- ravenpackapi/examples/get_document_url.py
+++ ravenpackapi/examples/get_document_url.py
@@ -2,12 +2,24 @@ from ravenpackapi import RPApi
 
 from ravenpackapi.exceptions import APIException
 
+product = "rpa"  # or "edge"
+
+# There are a few differences between RPA and EDGE:
+if product == "rpa":
+    us30_id = "us30"
+    RP_STORY_ID = "rp_story_id"
+    TITLE = "headline"
+else:
+    us30_id = "us30-edge"
+    RP_STORY_ID = "rp_document_id"
+    TITLE = "title"
+
 # initialize the API (here we use the RP_API_KEY in os.environ)
-api = RPApi()
+api = RPApi(product=product)
 
 # query the json endpoint for a dataset ***
 # use the public dataset with id 'us30'
-ds = api.get_dataset(dataset_id='us30')
+ds = api.get_dataset(dataset_id=us30_id)
 
 data = ds.json(
     start_date='2019-08-05 18:00:00',
@@ -16,7 +28,7 @@ data = ds.json(
 
 last_rp_story_id = None
 for record in list(data)[:5]:  # get url of the first documents
-    rp_story_id = record['rp_story_id']
+    rp_story_id = record[RP_STORY_ID]
     if last_rp_story_id is None or last_rp_story_id != rp_story_id:
         try:
             url = api.get_document_url(rp_story_id)
@@ -27,6 +39,6 @@ for record in list(data)[:5]:  # get url of the first documents
                 raise
         last_rp_story_id = rp_story_id
     print(rp_story_id,
-          record['headline'],
+          record[TITLE],
           url,
           )
